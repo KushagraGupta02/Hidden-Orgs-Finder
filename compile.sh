e4dffee3d0d837695fd11473b4c7bf5a01263d2a
KushagraGupta02 <109025706+KushagraGupta02@users.noreply.github.com>
2023-10-07 14:10:10 +0530

    Add files via upload

---
+++ compile.sh
@@ -0,0 +1,2 @@
+g++ -std=c++11 -o main source/parser2.cpp
+g++ -std=c++11 -o printoutput source/printoutput.cpp 
---
+++ run1.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <argument>"
+  exit 1
+fi
+
+./main "$1"
